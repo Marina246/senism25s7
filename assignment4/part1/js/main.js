32737410c9f90e79a99d5e70ca74bc5020fddd03
Marina246 <rayden147s@gmail.com>
2025-07-16 14:52:35 -0400

    a4p1 sections: Basic Setup: and Initial variables and functions:
    Script tag and source to main.js were added to p1.html. Additionally Initial variables and functions were put in the main.js file.

---
+++ assignment4/part1/js/main.js
@@ -0,0 +1,30 @@
+/*
+Name: Marina Senis
+Student Number: 100774596
+File: main.js
+Date: July 16th, 2025
+Assignment 4 part 1 js file
+*/
+
+// Initial variables and functions:
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+// String for story text
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+// Arrays for inserts
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+
+
+
+
