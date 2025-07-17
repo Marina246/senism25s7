0b93b16cf8b0a56164e4bc5511bad060c5d32cc3
Marina246 <rayden147s@gmail.com>
2025-07-17 13:56:19 -0400

    a4p2: declared variables
    Declaring the array of image filenames,
    Declaring the alternative text for each image file

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -1,3 +1,11 @@
+/*
+Name: Marina Senis
+Student Number: 100774596
+File: main.js
+Date: July 17th, 2025
+Assignment 4 part 2 js file
+*/
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -5,8 +13,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const fileNames = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const altText = {
+  "pic1.jpg": "Closeup of blue eye",
+  "pic2.jpg": "Wave pattern on rocks",
+  "pic3.jpg": "Purple and white flowers",
+  "pic4.jpg": "Pharaohs tomb inscyptions",
+  "pic5.jpg": "Butterfly on leaf"
+};
 
 /* Looping through images */
 
